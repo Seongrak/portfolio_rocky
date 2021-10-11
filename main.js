af18d000e30355c05952010985852d92278dcd1d
rocky <rock9247@naver.com>
2021-10-10 20:35:49 -0400

    JavaScript, navbar,scroll

---
+++ main.js
@@ -0,0 +1,26 @@
+"use strict";
+
+// nabvar 투명(transparent)일때 스크롤 -> background color
+const navbar = document.querySelector("#navbar");
+const navbarHeight = navbar.getBoundingClientRect().height;
+
+document.addEventListener("scroll", () => {
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add("navbar--dark");
+  } else {
+    navbar.classList.remove("navbar--dark");
+  }
+});
+
+// navbar 메뉴 click
+const navbarMenu = document.querySelector(".navbar_menu");
+navbarMenu.addEventListener("click", (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+});
